40e329c1d3c5cf2c926b29f4e23ff35c77026387
khuranapranav30-collab <khurana.pranav30@gmail.com>
2026-09-18 22:39:45 +0530

    Refactor main.jsx to use App component

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
-import NavBar from './NavBar.jsx'
-import Banner from './Banner.jsx'
-import Profile from './Profile.jsx'
-import Gallery from './Gallery.jsx'
-import Articles from './Articles.jsx'
-import Tutorials from './Tutorials.jsx'
-import Footer from './Footer.jsx'
+
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <title>Home Page</title>
-    <NavBar />
-    <Banner />
-    <Profile />
-    <Gallery />
-    <Articles />
-    <Tutorials />
-    <Footer />
+    <App />
   </StrictMode>
 )
