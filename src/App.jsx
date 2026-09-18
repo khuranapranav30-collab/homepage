b0eef7c0c9b203e6b4aea9b6dabc24d127658c39
khuranapranav30-collab <khurana.pranav30@gmail.com>
2026-09-18 22:39:18 +0530

    Refactor App component to include multiple imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css'
 
-function App() {
+import NavBar from './NavBar.jsx'
+import Banner from './Banner.jsx'
+import Profile from './Profile.jsx'
+import Gallery from './Gallery.jsx'
+import Articles from './Articles.jsx'
+import Tutorials from './Tutorials.jsx'
+import Footer from './Footer.jsx'
 
+function App() {
   return (
     <>
-      
+      <NavBar />
+      <Banner />
+      <Profile />
+      <Gallery />
+      <Articles />
+      <Tutorials />
+      <Footer />
     </>
   )
 }
